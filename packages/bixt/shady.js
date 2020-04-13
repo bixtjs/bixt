a98f8a1915a1257f85f86da2d238237406a79a83
Ganesha <reekoheek@gmail.com>
2020-04-13 12:15:31 +0700

    fix #2

--- packages/bixt/shady.js
+++ packages/bixt/shady.js
@@ -1,3 +1,9 @@
+/**
+ * Create shady element
+ *
+ * @param {object} Element element to wrap
+ * @returns {Element}
+ */
 export function shady (Element) {
   class ShadyElement extends Element {
     createRenderRoot () {
